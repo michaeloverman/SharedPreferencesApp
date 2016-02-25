eb2dd17af68c5503d53acefc997cff5334823993
michaeloverman <overman.michael@gmail.com>
2016-02-25 01:18:43 -0500

    EditText added. Persists through rotation.

--- app/src/main/java/com/overman/sharedpreferencesapp/MainActivity.java
+++ app/src/main/java/com/overman/sharedpreferencesapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.overman.sharedpreferencesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.editText);
     }
 }
